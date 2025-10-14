4ee4b82fbd72bb01e6a51a78cce20db567fb8d0d
Rasheed Ballogan <20243526@s.ubaguio.edu>
2025-10-14 20:00:26 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,15 @@ import { TaskFilterComponent } from './task-filter/task-filter.component';
 
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet,
     TaskFormComponent,
     TaskListComponent,
     TaskFilterComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-=======
-  imports: [RouterOutlet, CommonModule, TaskFormComponent, TaskListComponent],
-  template: `
-  <h1>🗂️ Task Manager</h1>
-  <app-task-form></app-task-form>
-  <app-task-list></app-task-list>
-  
-`,
->>>>>>> 4e5349e21f3fbfb7c43dcaa6dc0bea4a972befbe
 })
 export class AppComponent {
   constructor(public taskService: TaskService) {}
